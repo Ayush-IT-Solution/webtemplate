bdc413671278372fd1e8d390baf99f89c2523e33
Ayush Singh <48611956+ayushkssk@users.noreply.github.com>
2021-08-12 16:31:13 +0530

    file update

---
+++ external/front/assets/js/custom1.js
@@ -0,0 +1,34 @@
+
+/* readmore*/
+$(document).ready(function(){
+  $(".btn").click(function(){
+    $(".text1").toggle();
+  });
+});
+/* serivices*/
+
+/*$("#tabs").hide();
+$("#link").click(function () {
+    $("#tabs").toggle();
+});
+
+
+$(document).ready(function(){
+  let scroll_link = $('.scroll');
+ 
+   //smooth scrolling -----------------------
+   scroll_link.click(function(e){
+       e.preventDefault();
+       let url = $('body').find($(this).attr('href')).offset().top;
+       $('html, body').animate({
+         scrollTop : url
+       },700);
+       $(this).parent().addClass('active');
+       $(this).parent().siblings().removeClass('active');
+       return false;	
+    });
+ });*/
+
+
+
+ 
